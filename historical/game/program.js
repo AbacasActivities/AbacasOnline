faa5cb2771b8129725e83b74663fef6c3dec2773
AbacasActivities <93754377+AbacasActivities@users.noreply.github.com>
2022-12-04 22:01:30 -0600

    I have uploaded my aurora GUI for my website as the main page

---
+++ historical/game/program.js
@@ -0,0 +1,13 @@
+let name = prompt("Type a name:");
+let noun1 = prompt("Type an object:");
+let place = prompt("Type a store name:");
+let action = prompt("Type a past tense action:");
+let noun2 = prompt("Type an object:")
+let job = prompt("Type a job:");
+let noun3 = prompt("Type a plural object:");
+let adjec = prompt("Type a descriptive word:");
+let noun4 = prompt("Type a plural object:");
+let ptv = prompt("Type a state of condition:");
+let yell = prompt("Type an exclamation:")
+
+alert(name + " rode their " + noun1 + " down to the " + place + " where they " + action + " all of the " + noun2 + "'s on the shelves. \"" + yell + "!\" all the " + job + "'s yelled as they rushed over to try and stop them, but it was too late. All of the " + noun3 + " were " + adjec + " and the " + noun4 + " were " + ptv + ". " + name + " would be back again, but it was now time to rebuild.")
