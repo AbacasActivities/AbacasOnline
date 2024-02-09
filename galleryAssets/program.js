6b1b9a74b7de96f2e6340cae252904ab31eac4ac
AbacasActivities <93754377+AbacasActivities@users.noreply.github.com>
2024-02-08 22:36:28 -0600

    Updated gallery! descriptions of images added.

--- galleryAssets/program.js
+++ galleryAssets/program.js
@@ -11,6 +11,18 @@ function next() {
  	document.getElementById("3").innerHTML = '<img style="width:200px;" src="' + totClicksNext + '.png" width="142px">';
     document.getElementById("2").innerHTML = '<img style="width:450px;" src="' + totClicks + '.png" width="290px">';
     document.getElementById("1").innerHTML = '<img style="width:200px;" src="' + totClicksLast + '.png" width="142px">';
+	
+	if(totClicks == -1) {
+		document.getElementById("description").innerHTML = 'A business woman which I may make into an actual character later down the line.';
+	}
+	
+	if(totClicks == 0) {
+		document.getElementById("description").innerHTML = 'The first color rendition of Daisy from my manga fully transformed I actually liked.';
+	}
+	
+	if(totClicks == 1) {
+		document.getElementById("description").innerHTML = 'A sketch of an outfit I found online. I improvised the head, as I felt leaving the figure headless was weird.';
+	}
 }
 
 /*The code below is for the back button, reducing the page variable, and putting the image tag with the edited page source*/
@@ -23,6 +35,19 @@ function back() {
  	document.getElementById("3").innerHTML = '<img style="width:200px;" src="' + totClicksNext + '.png" width="142px">';
     document.getElementById("2").innerHTML = '<img style="width:450px" src="' + totClicks + '.png" width="290px">';
     document.getElementById("1").innerHTML = '<img style="width:200px;" src="' + totClicksLast + '.png" width="142px">';
+	
+	if(totClicks == -1) {
+		document.getElementById("description").innerHTML = 'A business woman which I may make into an actual character later down the line.';
+	}
+	
+	if(totClicks == 0) {
+		document.getElementById("description").innerHTML = 'The first color rendition of Daisy from my manga fully transformed I actually liked.';
+	}
+	
+	if(totClicks == 1) {
+		document.getElementById("description").innerHTML = 'A sketch of an outfit I found online. I improvised the head, as I felt leaving the figure headless was weird.';
+	}
 }
+
 
 
